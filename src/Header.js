f06b7fa6d9d162caa37b36f87f0c3a22a24a63fc
zhengjunan <1029871348@qq.com>
2021-07-13 10:49:46 +0900

    app init

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>
+        this is the header
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
